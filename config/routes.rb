71bc9fd9be8e0ccf13dfb689d550c603b089cced
Filbert Daniel <filbertdan67@gmail.com>
2024-03-14 16:39:59 +0300

    Add resources to modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
   root "articles#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/articles", to: "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
 
   # Defines the root path route ("/")
   # root "posts#index"
