d3eb70bd70e1d7e2030971505b60267bf63a30eb
Filbert Daniel <filbertdan67@gmail.com>
2024-03-14 15:41:47 +0300

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/articles", to: "articles#index"
 
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
